ff7dd82708a351b75bb73176bbb4c8b537e2ce91
Irfan Rafif <mirfanrafif17@gmail.com>
2019-10-22 20:55:01 +0700

    selesaikan jobsheet 8

--- Jobsheet 8.sql
+++ Jobsheet 8.sql
@@ -144,3 +144,77 @@ FROM
     SalesMonth2007;
 
 --Soal no. 11
+WITH
+    SalesMonth2007
+    as
+    (
+        SELECT
+            MONTH(orderdate) as monthno,
+            -- AVG(val) OVER(PARTITION BY orderdate)  AS avgsalesmonth,
+            SUM(val) AS val
+
+        FROM
+            Sales.OrderValues
+        WHERE YEAR(orderdate) = 2007
+        GROUP BY MONTH(orderdate)
+    )
+SELECT
+    monthno,
+    val,
+    (
+        LAG(val, 1) OVER(ORDER BY monthno) + 
+        LAG(val, 2) OVER(ORDER BY monthno) + 
+        LAG(val, 3) OVER(ORDER BY monthno)
+    )/3 as avglast3month,
+    val - FIRST_VALUE(val) OVER(ORDER BY monthno ROWS UNBOUNDED PRECEDING) AS diffjan,
+    LEAD(val) OVER(ORDER BY monthno) as nextval
+FROM
+    SalesMonth2007
+
+--Soal no. 12
+SELECT
+    custid,
+    orderid,
+    orderdate,
+    val,
+    100. * val / SUM(val) OVER(PARTITION BY custid) as percoftotalcust
+FROM
+    Sales.OrderValues
+ORDER BY custid, percoftotalcust DESC;
+;
+
+--Soal no. 13
+SELECT
+    custid,
+    orderid,
+    orderdate,
+    val,
+    100. * val / SUM(val) OVER(PARTITION BY custid) as percoftotalcust,
+    SUM(val) OVER (PARTITION BY custid ORDER BY orderdate, orderid 
+ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)as runval
+FROM
+    Sales.OrderValues
+ORDER BY custid;
+;
+
+--Soal no. 14
+WITH
+    SalesMonth2007
+    as
+    (
+        SELECT
+            MONTH(orderdate) as monthno,
+            -- AVG(val) OVER(PARTITION BY orderdate)  AS avgsalesmonth,
+            SUM(val) AS val
+
+        FROM
+            Sales.OrderValues
+        WHERE YEAR(orderdate) = 2007
+        GROUP BY MONTH(orderdate)
+    )
+SELECT
+    monthno,
+    val,
+    AVG(val)OVER(ORDER BY monthno ROWS BETWEEN 3 PRECEDING AND CURRENT ROW) AS ytdval
+FROM
+    SalesMonth2007;
